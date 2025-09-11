bd455ed3e063f44987281911f98cfeda487f344c
Gem Capalad <gabrielmark.a.capalad@gmail.com>
2025-09-11 11:23:11 +0800

    Refactor birthday replacement function and add additional data validation functions

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
-def replace_invalid(bday):
-    df = pd.read_excel("data/new_member_data.xlsx")
-
+def replace_invalid_birthday(df, bday):
     invalid = df["birthday"].astype(str).str.contains(r"\.")
     invalid_indices = df[invalid].index
 
@@ -11,10 +9,47 @@ def replace_invalid(bday):
 
     df.to_excel("data/test.xlsx", index=False)
 
+def filter_tin(df):
+    valid = df[df["tin_no"].astype(str).str.len() != 11]
+
+    valid[["family_name", "first_name", "tin_no"]].to_excel("data/invalid-tin.xlsx")
+
+
+    print(valid[["family_name", "first_name", "tin_no"]])
+
+def no_bday(df):
+    print(df[df["birthday"].isna()])
+
+def tin_checker(df):
+    invalid_tin = ~df["tin_no"].astype(str).str.match(r"^\d{3}-\d{3}-\d{3}$")
+
+    print(df.loc[invalid_tin, "tin_no"])
+
+def replace_invalid_tin(df):
+    df["tin_no"] = df["tin_no"].astype(str)
+
+    df["tin_no"] = df["tin_no"].astype(str).str.replace(r"^(\d{3})(\d{3})(\d{3})$", r"\1-\2-\3", regex=True)
+
+    df.to_excel("data/old_member_test.xlsx", index=False)
+
+def email_checker(df):
+    invalid_mask = df["email"].astype(str).str.contains("/") & df["email"].astype(str).str.contains("@")
+
+    print(df.loc[invalid_mask, ["family_name", "first_name", "email"]])
+
 def main():
+    old_df = pd.read_excel("data/old_member_data.xlsx")
+    new_df = pd.read_excel("data/test.xlsx")
+    test_df = pd.read_excel("data/old_member_test.xlsx")
+
     proper_birthdays = [] # i put d bdays here
     
-    replace_invalid(proper_birthdays)
-    
+    # replace_invalid_birthday(new_df, proper_birthdays)
+    # filter_tin(new_df)
+    # no_bday(new_df)
+    # tin_checker(test_df)
+    # replace_invalid_tin(old_df)
+    email_checker(test_df)
+
 if __name__ == '__main__':
     main()
