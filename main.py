3ee688c33f1982b23960655a6707294f8b1dd4b5
Gem Capalad <gabrielmark.a.capalad@gmail.com>
2025-09-18 13:05:48 +0800

    Refactor email validation and enhance number processing functions

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def filter_tin(df):
 
     valid[["family_name", "first_name", "tin_no"]].to_excel("data/invalid-tin.xlsx")
 
-
     print(valid[["family_name", "first_name", "tin_no"]])
 
 def no_bday(df):
@@ -33,14 +32,65 @@ def replace_invalid_tin(df):
     df.to_excel("data/old_member_test.xlsx", index=False)
 
 def email_checker(df):
-    invalid_mask = df["email"].astype(str).str.contains("/") & df["email"].astype(str).str.contains("@")
+    invalid_mask = ~df["email"].astype(str).str.contains("@", na=False)
 
-    print(df.loc[invalid_mask, ["family_name", "first_name", "email"]])
+    df.loc[invalid_mask, "email"] = ""
+
+    df.to_excel("data/cleaned_emails.xlsx", index=False)
+
+def email_reader(df):
+    invalid_mask = ~df["email"].astype(str).str.contains("@", na=False)
+
+    print(df.loc[invalid_mask, ["first_name", "family_name", "email"]])
+
+def number_cleaner(df):
+    df["digits_only"] = df["contact_no"].astype(str).str.replace(r"\D", "", regex=True)
+
+    mask = (df["digits_only"].str.len() > 0) & (df["digits_only"].str.len() != 11)  
+
+    empty_landline_mask = df["landline"].isna()
+
+    final_mask = mask & empty_landline_mask
+
+    df.loc[final_mask, "landline"] = df.loc[final_mask, "contact_no"]
+    df.loc[mask, "contact_no"] = ""
+
+    df = df.drop(columns=["digits_only"])
+
+    df.to_excel("data/old_version_4.xlsx", index=False)
+
+def number_checker(df):
+    df["digits_only"] = df["contact_no"].astype(str).str.replace(r"\D", "", regex=True)
+
+    mask = (df["digits_only"].str.len() != 11) & (df["digits_only"].str.len() > 0)
+
+    print("digits only")
+    invalid_contacts = df.loc[mask, ["family_name", "contact_no", "digits_only"]]
+    print(invalid_contacts)
+
+    [print("empty landlines")]
+    empty_landline_mask = df["landline"].isna()
+    print(df.loc[empty_landline_mask, ["family_name", "landline"]])
+
+    print("final")
+    final_mask = mask & empty_landline_mask
+    print(df.loc[final_mask, ["family_name", "landline"]])
+
+def number_landline_checker(df):
+    mask = (df["contact_no"].isna()) & (df["landline"].isna())
+    print(df.loc[mask, ["family_name", "first_name", "contact_no", "landline"]])
+
+def number_dash_remover(df):
+    df["contact_no"] = df["contact_no"].str.replace("-", "", regex=False)
+    df.to_excel("data/old_version_5.xlsx", index=False)
+
+def landline_checker(df):
+    print(df.loc[df["landline"].astype(str).str.match(r"^\(\d{3}\)\d{3}-\d{4}$"), ["family_name", "landline"]])
 
 def main():
-    old_df = pd.read_excel("data/old_member_data.xlsx")
-    new_df = pd.read_excel("data/test.xlsx")
-    test_df = pd.read_excel("data/old_member_test.xlsx")
+    old_df = pd.read_excel("data/old_version_1.xlsx")
+    new_df = pd.read_excel("data/new_version_2.xlsx")
+    test_df = pd.read_excel("data/old_version_5.xlsx")
 
     proper_birthdays = [] # i put d bdays here
     
@@ -49,7 +99,11 @@ def main():
     # no_bday(new_df)
     # tin_checker(test_df)
     # replace_invalid_tin(old_df)
-    email_checker(test_df)
+    # email_reader(test_df)
+    # number_checker(test_df)
+    # number_landline_checker(test_df)
+    # number_dash_remover(test_df)
+    landline_checker(test_df)
 
 if __name__ == '__main__':
     main()
